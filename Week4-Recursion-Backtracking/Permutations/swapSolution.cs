8202113b34371f782740cd6cbaea6e08347c9543
Shin <cnagapraneeth@gmail.com>
2025-01-04 17:38:52 +0530

    Swap solution for Permutations Problem

---
+++ Week4-Recursion-Backtracking/Permutations/swapSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations
+{
+    public class Solution
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            Permutation(nums, 0, result);
+            return result;
+        }
+
+        private void Permutation(int[] nums, int index, IList<IList<int>> result)
+        {
+            if (index == nums.Length)
+            {
+                result.Add(new List<int>(nums));
+                return;
+            }
+
+            for (int i = index; i < nums.Length; i++)
+            {
+                Swap(nums, index, i);
+                Permutation(nums, index + 1, result);
+                Swap(nums, index, i);
+            }
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
