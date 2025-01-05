eef55251bf89d149dfa3ea029399ba04e6023bdd
Shin <cnagapraneeth@gmail.com>
2025-01-05 13:23:00 +0530

    Premutation II

---
+++ Week3-Array-String/FindMirrorScoreOfAString/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace FindMirrorScoreOfAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aczzx";
+            Solution solution = new Solution();
+            Console.WriteLine(solution.CalculateScore(s));
+        }
+        public class Solution
+        {
+            public long CalculateScore(string s)
+            {
+                Dictionary<char, List<int>> dic = new();
+
+                int totalScore = 0;
+
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    if (!dic.ContainsKey(s[i]))
+                    {
+                        dic[s[i]] = new List<int>();
+                    }
+                    dic[s[i]].Add(i);
+                }
+
+                for (int j = 1; j < s.Length; j++)
+                {
+
+                    char currentChar = s[j];
+                    char reversedChar = (char)('z' - (currentChar - 'a'));
+
+                    if (!dic.ContainsKey(reversedChar) || dic[reversedChar].Count == 0)
+                    {
+                        continue;
+                    }
+
+
+                    int closestIndex = -1;
+
+                    for (int i = 0; i < dic[reversedChar].Count; i++)
+                    {
+                        if (dic[reversedChar][i] < j)
+                        {
+                            closestIndex = dic[reversedChar][i];
+                            dic[reversedChar].RemoveAt(i);
+                            dic[currentChar].Remove(j);
+                            break;
+                        }
+                    }
+
+                    if (closestIndex == -1) continue;
+                    totalScore += (j - closestIndex);
+                }
+                return totalScore;
+            }
+        }
+    }
+}
---
+++ Week4-Recursion-Backtracking/PermutationsII/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace PermutationsII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1];
+            Solution solution = new Solution();
+            var x = solution.PermuteUnique(nums);
+        }
+        public class Solution
+        {
+            public IList<IList<int>> PermuteUnique(int[] nums)
+            {
+                Dictionary<int, int> dic = new();
+                int n = nums.Length;
+                List<int> keys = new();
+
+                foreach (int i in nums)
+                {
+                    if (!dic.ContainsKey(i))
+                    {
+                        dic[i] = 0;
+                        keys.Add(i);
+                    }
+                    dic[i]++;
+                }
+
+                List<IList<int>> result = new();
+                List<int> tempList = new();
+
+                void BackTrack(int startIndex, List<int> templi)
+                {
+                    if (templi.Count == n)
+                    {
+                        result.Add(new List<int>(templi));
+                        return;
+                    }
+
+                    for(int i = startIndex; i < keys.Count; i++)
+                    {
+                        if (dic[keys[i]] == 0) continue;
+                        templi.Add(keys[i]);
+                        dic[keys[i]]--;
+                        BackTrack(startIndex, templi);
+                        templi.RemoveAt(templi.Count - 1);
+                        dic[keys[i]]++;
+                    }
+                }
+
+                BackTrack(0, tempList);
+                return result;
+            }
+        }
+    }
+}
