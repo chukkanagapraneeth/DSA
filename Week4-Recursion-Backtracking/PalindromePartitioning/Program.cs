4a370849e142984e23a4878b9c93c0ac8bf550af
Shin <cnagapraneeth@gmail.com>
2025-01-08 18:21:46 +0530

    Palindrome Partitioning

---
+++ Week4-Recursion-Backtracking/PalindromePartitioning/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace PalindromePartitioning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aab";
+            Solution solution = new Solution();
+            var x = solution.Partition(s);
+        }
+        public class Solution
+        {
+            public IList<IList<string>> Partition(string s)
+            {
+                List<IList<string>> result = new();
+
+                List<string> tempList = new();
+
+                void BackTrack(int idx, List<string> currentPartition)
+                {
+                    if(idx == s.Length)
+                    {
+                        result.Add(new List<string>(currentPartition));
+                        return;
+                    }
+
+                    for(int i = idx; i < s.Length; i++)
+                    {
+                        if (IsPalindrome(s, idx, i))
+                        {
+                            currentPartition.Add(s.Substring(idx, i - idx + 1));
+                            BackTrack(i + 1, currentPartition);
+                            currentPartition.RemoveAt(currentPartition.Count - 1);
+                        }
+                    }
+                }
+
+                BackTrack(0, tempList);
+                return result;
+
+            }
+
+            private bool IsPalindrome(string s, int start, int end)
+            {
+                while(start < end)
+                {
+                    if (s[start] != s[end]) return false;
+                    start++;
+                    end--;
+                }
+                return true;
+            }
+        }
+    }
+}
