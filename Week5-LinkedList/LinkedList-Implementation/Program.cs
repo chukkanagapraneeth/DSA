3b36df0f12c21222c95158db717fff2dce06ab82
Shin <cnagapraneeth@gmail.com>
2025-01-20 23:32:40 +0530

    Linked List Implementation & Palindrome Linked List

---
+++ Week5-LinkedList/LinkedList-Implementation/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace LinkedList_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            LinkedList<string> linkedList = new LinkedList<string>();
+            linkedList.Add("Ram");
+            linkedList.Add("Praneeth");
+            linkedList.Print();
+        }
+
+        public class LinkedList<T> 
+        {
+            private class Node
+            {
+                public T Data { get; set; }
+                public Node Next { get; set; }
+                public Node(T data)
+                {
+                    Data = data;
+                    Next = null;
+                }
+            }
+
+            private Node head;
+            private int count;
+
+            public LinkedList()
+            {
+                head = null;
+                count = 0;
+            }
+
+            public void Add(T data)
+            {
+                Node newNode = new Node(data);
+                if(head == null)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    Node currentNode = head;
+                    while(currentNode.Next != null)
+                    {
+                        currentNode = currentNode.Next;
+                    }
+                    currentNode.Next = newNode;
+                }
+                count++;
+            }
+
+            public void Print()
+            {
+                StringBuilder sb = new StringBuilder();
+                Node currentNode = head;
+                while(currentNode != null)
+                {
+                    sb.Append(currentNode.Data).Append(" ---> ");
+                    currentNode = currentNode.Next;
+                }
+                sb.Append("null");
+
+                Console.WriteLine(sb.ToString());
+            }
+        }
+    }
+}
---
+++ Week5-LinkedList/PalindromeLinkedList/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace PalindromeLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        /**
+         * Definition for singly-linked list.
+         * public class ListNode {
+         *     public int val;
+         *     public ListNode next;
+         *     public ListNode(int val=0, ListNode next=null) {
+         *         this.val = val;
+         *         this.next = next;
+         *     }
+         * }
+         */
+        public class Solution
+        {
+            public bool IsPalindrome(ListNode head)
+            {
+                List<int> li = new();
+                ListNode currentNode = head;
+                while (currentNode != null)
+                {
+                    li.Add(currentNode.val);
+                    currentNode = currentNode.next;
+                }
+
+                int start = 0;
+                int end = li.Count - 1;
+
+                while (start < end)
+                {
+                    if (li[start] != li[end])
+                    {
+                        return false;
+                    }
+                    start++; end--;
+                }
+
+                return true;
+            }
+        }
+    }
+}
